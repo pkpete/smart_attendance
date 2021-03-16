b5a21296e6050d96c61ef94ca4ba7fc9bd8ef3df
mindu96 <kgsmyung1@gmail.com>
2021-03-16 16:35:40 +0900

    start project

---
+++ README.md
@@ -0,0 +1,2 @@
+# smart_attendance
+graduation project
---
+++ src/main/java/graduation_project/smart_attendance/SmartAttendanceApplication.java
@@ -0,0 +1,13 @@
+package graduation_project.smart_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceApplication.class, args);
+	}
+
+}
